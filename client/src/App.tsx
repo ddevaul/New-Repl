8669a8317722dea628a3ac4988bf553cb62f2157
dpd2136 <37639457-dpd2136@users.noreply.replit.com>
2024-12-12 21:05:34 +0000

    Agent query: Can you now access the /auth route and see the signup/login form?
    Add authentication page.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/65c7a6ef-2272-4502-90a0-fa2830eaca1c/8d5bcc3e-df32-42fc-8fe2-e4df27f632d1.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Switch, Route } from "wouter";
 import Home from "./pages/Home";
 import Room from "./pages/Room";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/room/:code" component={Room} />
+      <Route path="/auth" component={Auth} />
       <Route>404 - Page Not Found</Route>
     </Switch>
   );
